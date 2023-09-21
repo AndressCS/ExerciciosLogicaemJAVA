a9f1b401de1b667996666d8d54307827aa0713e9
Andress Silva <andresscvlcnt@gmail.com>
2023-09-20 22:07:17 -0300

    Poupanca Banco

---
+++ Exercicios/ExerciciosJAVA/Exer 5/PoupancaBanco/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("--------------------");
+        System.out.println("|-------BANCO------|");
+        System.out.println("--------------------");
+
+        System.out.print("Qual Foi o Valor Depositado: ");
+        float valor = teclado.nextFloat();
+        float ren = (float) ((0.07*valor)/100);
+        System.out.println("Após um Mês de Rendimento o Valor Será: " + (valor + ren));
+    }
+}
