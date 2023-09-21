5fe60b297c2d32b3138583fbd884618ade6f9759
Andress Silva <andresscvlcnt@gmail.com>
2023-09-20 22:08:43 -0300

    Poupanca Rendimento

--- Exercicios/ExerciciosJAVA/Exer 5/PoupancaBanco/src/App.java
+++ Exercicios/ExerciciosJAVA/Exer 5/PoupancaBanco/src/App.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    /**
-     * @param args
-     * @throws Exception
-     */
+    
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
         System.out.println("--------------------");
